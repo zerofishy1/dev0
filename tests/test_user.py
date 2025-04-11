e17809f6ef26c4f41b4b764ba06b97d83afa1605
Хамид Мусаев <hamidmusaev@MacBook-Pro-Hamid.local>
2025-04-11 12:20:44 +0300

    Ci/cd

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,3 +1,4 @@
+
 from fastapi.testclient import TestClient
 
 from src.main import app
@@ -26,16 +27,20 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': "anonim@mail.com"})
+    assert response.status_code == 404
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={'email': "lox@mail.com", "name": "lox"})
+    assert response.status_code == 201
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={'email': "lox@mail.com", "name": "lox"})
+    assert response.status_code == 409
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': "lox@mail.com"})
+    assert response.status_code == 204
